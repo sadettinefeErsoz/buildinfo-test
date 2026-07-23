795e0ec7b8a298db992174187a8a7a1280e28e9b
Sadettin Efe Ersöz <sadettinefe.rsz@gmail.com>
2026-07-23 12:48:11 +0300

    deneme

--- src/test_servis/hello.go
+++ src/test_servis/hello.go
@@ -4,5 +4,7 @@ import "fmt"
 
 func main() {
 	fmt.Println("Bu dosya buildinfo test senaryosu icin var.")
+	fmt.Println("ikinci degisiklik")
+	fmt.Println("kinci degisiklik")
+
 }
-fmt.Println("ikinci degisiklik")
